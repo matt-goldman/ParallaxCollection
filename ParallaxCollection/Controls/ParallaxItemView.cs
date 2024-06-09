e0882cb889d9aba08282a4cee94798c16fce75f2
Matt Goldman <matt.goldman@outlook.com>
2024-06-09 10:12:02 +1000

    Split platform specific code into platform folders

--- ParallaxCollection/Controls/ParallaxItemView.cs
+++ ParallaxCollection/Controls/ParallaxItemView.cs
@@ -1,86 +1,38 @@
-#if IOS
-using Foundation;
-#endif
-
-using ParallaxCollection.Helpers;
-
 namespace ParallaxCollection.Controls;
 
-public abstract class ParallaxItemView : ContentView
+public abstract partial class ParallaxItemView : ContentView
 {
-#if IOS
-    protected PositionCalculatingView positionCalculatingView;
-#endif
-    public ParallaxItemView()
-    {
-
-        Microsoft.Maui.Handlers.ContentViewHandler.Mapper.AppendToMapping("parallax", (handler, view) =>
-        {
-            if (view is ParallaxItemView pView)
-            {
-#if ANDROID
-                handler.PlatformView.ViewTreeObserver!.ScrollChanged += (s, e) =>
-                {
-                    int[] location = new int[2];
-                    handler.PlatformView.GetLocationOnScreen(location);
-                    int x = location[0];
-                    int y = location[1];
-
-                    pView?.UpdatePosition(y);
-                };
-#elif IOS
-                positionCalculatingView = new PositionCalculatingView
-                {
-                    ParentParallaxItemView = this
-                };
-
-                handler.PlatformView.AddSubview(positionCalculatingView);
-#endif
-            }
-        });
-
-#if ANDROID
-        _denominator = 10;
-#elif IOS
-        _denominator = 3;
-#endif
-    }
-
-    public void UpdatePosition(int y)
-    {
-        PlatformY = y;
-    }
-
     private int _y;
+    private int _denominator;
+    protected double ParallaxOffsetY;
+    private double ThisCenter;
+    private double CenterY;
+
     public int PlatformY
     {
         get => _y;
-        set
+        private set
         {
             _y = value;
             OnPropertyChanged();
         }
     }
 
-    private int _denominator;
-
-    protected double ParallaxOffsetY;
+    public ParallaxItemView()
+    {
+        ConfigurePlatform();
+    }
 
     public virtual void OnScrolled()
     {
         if (Height == -1)
             return;
 
-        double thisCentre = 0;
-#if IOS
-        positionCalculatingView.CalculatePosition();
-        thisCentre = PlatformY;
-#elif ANDROID
-        thisCentre = PlatformY + (Height / 2);
-#endif
-
-        var diff = thisCentre - PositionHelpers.CentreY;
-
+        CalculateCentre();
+        var diff = ThisCenter - CenterY;
         ParallaxOffsetY = diff / _denominator;
     }
+
+    partial void ConfigurePlatform();
+    partial void CalculateCentre();
 }
---
+++ ParallaxCollection/Platforms/Android/ParallaxItemView.Android.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Maui.Handlers;
+
+namespace ParallaxCollection.Controls;
+
+public partial class ParallaxItemView
+{
+    partial void ConfigurePlatform()
+    {
+        _denominator = 10;
+        CenterY = DeviceDisplay.MainDisplayInfo.Height / DeviceDisplay.MainDisplayInfo.Density / 2;
+
+        ContentViewHandler.Mapper.AppendToMapping("parallax", (handler, view) =>
+        {
+            if (view is ParallaxItemView pView)
+            {
+                handler.PlatformView.ViewTreeObserver!.ScrollChanged += (s, e) =>
+                {
+                    int[] location = new int[2];
+                    handler.PlatformView.GetLocationOnScreen(location);
+                    int x = location[0];
+                    int y = location[1];
+
+                    pView?.UpdatePlatformY(y);
+                };
+            }
+        });
+    }
+
+    partial void CalculateCentre()
+    {
+        ThisCenter = PlatformY + (Height / 2);
+    }
+
+    public void UpdatePlatformY(int y)
+    {
+        PlatformY = y;
+    }
+}
