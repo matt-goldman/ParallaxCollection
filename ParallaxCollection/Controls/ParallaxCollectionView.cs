b528c8907738c16c2901cd639ec551d3fcf42dd4
Matt Goldman [SSW] <mattgoldman@ssw.com.au>
2024-06-30 10:07:43 +1000

    restored child iteration in pcv

--- ParallaxCollection/Controls/ParallaxCollectionView.cs
+++ ParallaxCollection/Controls/ParallaxCollectionView.cs
@@ -2,32 +2,20 @@
 {
     public class ParallaxCollectionView : CollectionView
     {
-        private readonly List<ParallaxItemView> _parallaxItemViews = new List<ParallaxItemView>();
-
+        
         protected override void OnScrolled(ItemsViewScrolledEventArgs e)
         {
             base.OnScrolled(e);
-            foreach (var item in _parallaxItemViews)
-            {
-                item?.OnScrolled();
-            }
-        }
+            var vte = (IVisualTreeElement)this;
 
-        protected override void OnChildAdded(Element child)
-        {
-            base.OnChildAdded(child);
-            if (child is ParallaxItemView parallaxItemView)
-            {
-                _parallaxItemViews.Add(parallaxItemView);
-            }
-        }
+            var children = vte.GetVisualChildren();
 
-        protected override void OnChildRemoved(Element child, int oldIndex)
-        {
-            base.OnChildRemoved(child, oldIndex);
-            if (child is ParallaxItemView parallaxItemView)
+            foreach (var child in children)
             {
-                _parallaxItemViews.Remove(parallaxItemView);
+                if (child is ParallaxItemView card)
+                {
+                    card.OnScrolled();
+                }
             }
         }
     }
